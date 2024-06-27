27f936988b3aaa8f6d1503e31505d02fbf3ae149
Kacper Świątek <kacperswiatek99@gmail.com>
2024-06-27 13:26:25 +0200

    Changed Angular version to 17.3.8

--- StudyWire.Client/src/app/app.component.spec.ts
+++ StudyWire.Client/src/app/app.component.spec.ts
@@ -2,9 +2,11 @@ import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    declarations: [AppComponent]
-  }));
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [AppComponent],
+    }).compileComponents();
+  });
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
@@ -12,7 +14,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'StudyWire.Client'`, () => {
+  it(`should have the 'StudyWire.Client' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('StudyWire.Client');
@@ -22,6 +24,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('StudyWire.Client app is running!');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, StudyWire.Client');
   });
 });
